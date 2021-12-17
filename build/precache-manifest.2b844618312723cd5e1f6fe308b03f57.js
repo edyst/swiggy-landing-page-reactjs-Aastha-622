7b64ef00858f77c33a5f0dbb6c2c3c75fe199148
Aastha-622 <82527889+Aastha-622@users.noreply.github.com>
2021-12-17 18:41:08 +0530

    Add files via upload

---
+++ build/precache-manifest.2b844618312723cd5e1f6fe308b03f57.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a936156f0077993c363b",
+    "url": "./static/css/main.674a07eb.chunk.css"
+  },
+  {
+    "revision": "a936156f0077993c363b",
+    "url": "./static/js/main.a936156f.chunk.js"
+  },
+  {
+    "revision": "e58a7950ffa1916f4c37",
+    "url": "./static/js/1.e58a7950.chunk.js"
+  },
+  {
+    "revision": "d05d15b1d7d46cc0b1be",
+    "url": "./static/js/2.d05d15b1.chunk.js"
+  },
+  {
+    "revision": "777d3def79f0f7ae7739",
+    "url": "./static/js/runtime~main.777d3def.js"
+  },
+  {
+    "revision": "d231d68a66e00d31ed11f318882abb1e",
+    "url": "./static/media/Lun.d231d68a.jpeg"
+  },
+  {
+    "revision": "f9aba0b4ffd0d942ab68d35adc03883b",
+    "url": "./static/media/Swiggy_logo.f9aba0b4.png"
+  },
+  {
+    "revision": "ea67649e69a739d7b38bd82e92f59b7b",
+    "url": "./static/media/rsec1.ea67649e.png"
+  },
+  {
+    "revision": "7bf54649e4b428e8224d09c71b6f98f7",
+    "url": "./static/media/rsec2.7bf54649.png"
+  },
+  {
+    "revision": "6739bd47a92af3a265b9e510388e5cd2",
+    "url": "./static/media/Lunch.6739bd47.jpeg"
+  },
+  {
+    "revision": "1b111d95a30e267a5490231bc6c5dcfb",
+    "url": "./index.html"
+  }
+];
